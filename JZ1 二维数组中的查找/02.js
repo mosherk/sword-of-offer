a81cca8029b3db6d371e5e04524d977b54ccce03
墨流 <75362269+myaptx@users.noreply.github.com>
2021-09-27 00:36:03 +0800

    Create 02.js

---
+++ JZ1 二维数组中的查找/02.js
@@ -0,0 +1,20 @@
+//解法二：二分法  
+function Find(target, array)
+{
+    let m = array.length;
+    let n = array[0].length;
+    if(m===0 || n===0){
+        return false;
+    }
+    let r = 0 ,c = n-1;
+    while(r<m && c>=0){
+        if(target == array[r][c]){
+            return true
+        }else if(target > array[r][c]){
+            ++r;
+        }else{
+            --c;
+        }
+    }
+    return false;
+}
